6d1ccb6d56c29eb00dd566a89a206105055ad44e
anguske <anguske@aaxischina.com>
2025-03-21 11:26:20 +0800

    remove useless packages

--- components/shared/Excalidraw.tsx
+++ components/shared/Excalidraw.tsx
@@ -1,15 +1,6 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import { useAtom } from "jotai";
-import { themeState } from "@/utils/jotail";
-import Image from "next/image";
-import {
-  Excalidraw,
-  convertToExcalidrawElements,
-} from "@excalidraw/excalidraw";
-
+import React from "react";
 export default function ExcalidrawPage() {
-  const themeStateText = useAtom(themeState);
   return (
     <div style={{ height: "750px" }}>
       {/* <Excalidraw /> */}
