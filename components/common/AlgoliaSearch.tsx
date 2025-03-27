32ab94e7523972d73be3547ba440955df8aeda3a
anguske <anguske@aaxischina.com>
2025-03-27 10:33:11 +0800

    Enhance Algolia search with custom search box and translations

--- components/common/AlgoliaSearch.tsx
+++ components/common/AlgoliaSearch.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { algoliasearch } from "algoliasearch";
-import { InstantSearch, SearchBox, Hits } from "react-instantsearch";
+import { InstantSearch, SearchBox, Hits, useSearchBox } from "react-instantsearch";
 import { useTranslation } from "react-i18next";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -25,49 +25,52 @@ const Hit = ({ hit }: { hit: AlgoliaPost }) => (
   </Link>
 );
 
-export function AlgoliaSearch() {
+const CustomSearchBox = ({ onFocus, onBlur }: { onFocus: () => void; onBlur: () => void }) => {
   const { t } = useTranslation();
+  const { query, isSearchStalled } = useSearchBox();
+
+  return (
+    <SearchBox
+      placeholder={t("header.search")}
+      onFocus={onFocus}
+      onBlur={onBlur}
+      className={`[&_.ais-SearchBox-submit]:absolute [&_.ais-SearchBox-submit]:right-3 [&_.ais-SearchBox-submit]:top-1/2
+         [&_.ais-SearchBox-submit]:-translate-y-1/2 [&_.ais-SearchBox-submit]:text-gray-700
+         [&_.ais-SearchBox-submit]:dark:text-white [&_.ais-SearchBox-submit_svg]:w-4 [&_.ais-SearchBox-submit_svg]:h-5
+         [&_.ais-SearchBox-reset]:absolute [&_.ais-SearchBox-reset]:right-3
+         [&_.ais-SearchBox-reset]:top-1/2 [&_.ais-SearchBox-reset]:-translate-y-1/2 [&_.ais-SearchBox-reset]:text-gray-700
+         [&_.ais-SearchBox-reset]:dark:text-white [&_.ais-SearchBox-loadingIndicator]:absolute [&_.ais-SearchBox-loadingIndicator]:right-3
+         [&_.ais-SearchBox-loadingIndicator]:top-1/2 [&_.ais-SearchBox-loadingIndicator]:-translate-y-1/2 [&_.ais-SearchBox-loadingIndicator]:text-gray-700
+         [&_.ais-SearchBox-loadingIndicator]:dark:text-white [&_.ais-SearchBox-form]:relative [&_.ais-SearchBox-input]:w-full
+         [&_.ais-SearchBox-input]:px-4 [&_.ais-SearchBox-input]:py-2 [&_.ais-SearchBox-input]:rounded-lg
+         [&_.ais-SearchBox-input]:bg-gray-100 [&_.ais-SearchBox-input]:dark:bg-gray-800 [&_.ais-SearchBox-input]:text-gray-700
+         [&_.ais-SearchBox-input]:dark:text-gray-200 [&_.ais-SearchBox-input]:focus-visible:outline-none [&_.ais-SearchBox-input]: border-none
+         hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors duration-200 [&_.ais-SearchBox-input::-webkit-search-cancel-button]:hidden
+         ${isSearchStalled ? '[&_.ais-SearchBox-submit]:hidden [&_.ais-SearchBox-reset]:hidden' :
+           query ? '[&_.ais-SearchBox-submit]:hidden [&_.ais-SearchBox-loadingIndicator]:hidden' : 
+           '[&_.ais-SearchBox-reset]:hidden [&_.ais-SearchBox-loadingIndicator]:hidden'}`}
+    />
+  );
+};
+
+export function AlgoliaSearch() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="relative">
-      <button
-        onClick={() => setIsOpen(!isOpen)}
-        className="flex items-center gap-2 px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-200 hover:text-gray-900 dark:hover:text-white"
+      <InstantSearch
+        searchClient={searchClient}
+        indexName={process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME || "posts"}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
-        {t("header.search")}
-      </button>
-
-      {isOpen && (
-        <div className="absolute left-0 mt-2 w-96 bg-white dark:bg-gray-800 rounded-lg shadow-lg p-4 z-50 border border-gray-200 dark:border-gray-700">
-          <InstantSearch 
-            searchClient={searchClient} 
-            indexName={process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME || "posts"}
-          >
-            <SearchBox
-              placeholder={t("header.searchPlaceholder")}
-              className="mb-4 [&_.ais-SearchBox-form]:relative [&_.ais-SearchBox-input]:w-full [&_.ais-SearchBox-input]:px-4 [&_.ais-SearchBox-input]:py-2 [&_.ais-SearchBox-input]:rounded-lg [&_.ais-SearchBox-input]:border [&_.ais-SearchBox-input]:border-gray-300 [&_.ais-SearchBox-input]:dark:border-gray-600 [&_.ais-SearchBox-input]:bg-white [&_.ais-SearchBox-input]:dark:bg-gray-700 [&_.ais-SearchBox-input]:text-gray-900 [&_.ais-SearchBox-input]:dark:text-white [&_.ais-SearchBox-input]:focus:outline-none [&_.ais-SearchBox-input]:focus:ring-2 [&_.ais-SearchBox-input]:focus:ring-blue-500 [&_.ais-SearchBox-input]:dark:focus:ring-blue-400"
-            />
+        <CustomSearchBox onFocus={() => setIsOpen(true)} onBlur={() => setTimeout(() => setIsOpen(false), 200)} />
+        {isOpen && (
+          <div className="absolute left-0 mt-2 w-96 bg-white dark:bg-gray-800 rounded-lg shadow-lg p-4 z-50 border border-gray-200 dark:border-gray-700">
             <div className="max-h-[400px] overflow-y-auto">
               <Hits hitComponent={Hit} />
             </div>
-          </InstantSearch>
-        </div>
-      )}
+          </div>
+        )}
+      </InstantSearch>
     </div>
   );
 } 
